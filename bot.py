2018dfa8c9addf82125d235f5a4f24a31e92bc85
Nokkum <nokkum.github@gmail.com>
2025-10-31 13:35:24 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,8 +12,8 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    channel = discord.utils.get(member.guild.text_channels, name="👋-》welcome")
+    channel = discord.utils.get(member.guild.text_channels, name="welcome")
     if channel:
-        await channel.send(f"Welcome, {member.mention}! Be sure to check out {#1372767539863748698}")
+        await channel.send(f"Welcome, {member.mention}! Be sure to check out {rules_channel.mention}")
 
 bot.run(os.getenv("MTQzMzY2OTUwOTcwMDY0ODk2MA.G4OpEP.bVX2G4-dTb4v-UXf5dqmuGga4KikAbgQWUdS50"))
